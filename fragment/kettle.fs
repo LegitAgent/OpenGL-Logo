939e06c224b21c6050dfbcea7447dd53860055ee
DyloreAquino <lutherjerold@gmail.com>
2026-02-18 16:27:26 +0800

    Demos and Changes

--- fragment/kettle.fs
+++ fragment/kettle.fs
@@ -11,24 +11,10 @@
 
 #version 330 core
 
-uniform float time;
-in vec2 uv;
 in vec3 shaderColor;
 out vec4 fragmentColor;
 
 void main()
 {
-    vec3 darkSilver = vec3(0.15, 0.15, 0.18);
-    vec3 darkViolet = vec3(0.07, 0.0, 0.09);
-
-    float speed = 2.0;
-    float interval = 2.0;
-    float wave = sin(uv.y * interval + time * speed);
-
-    float t = wave * 0.5 + 0.5;
-    
-    // exponential average!
-    vec3 color = darkSilver * (1.0 - t) + darkViolet * t;
-
-    fragmentColor = vec4(color, 1.0);
+    fragmentColor = vec4(shaderColor, 1.0f);
 }
--- vertex/kettle.vs
+++ vertex/kettle.vs
@@ -12,12 +12,9 @@ layout (location = 0) in vec3 vertexPosition;
 layout (location = 1) in vec3 vertexColor;
 
 out vec3 shaderColor;
-out vec2 uv;
 
 void main()
 {
     gl_Position = vec4(vertexPosition, 1.0f);
-    uv.x = gl_Position.x;
-    uv.y = gl_Position.y;
     shaderColor = vertexColor;
 }
