8c1ac3679b4c04cfc713bb25323b6ec3497f829f
Alba Martin <albamartindarius@gmail.com>
2026-03-03 22:04:36 +0800

    sorted java files and edited bezier curve algorithm printing

---
+++ java/Point2D.java
@@ -0,0 +1,18 @@
+
+public class Point2D {
+
+    double x, y;
+
+    public Point2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
