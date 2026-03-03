ff962fe29da2d76ce73f627644d48f788990f18d
thisotherguyalso <161091156+thisotherguyalso@users.noreply.github.com>
2026-03-03 21:31:21 +0800

    Generate Bezier works now

--- GenerateBezierCurve.java
+++ GenerateBezierCurve.java
@@ -3,24 +3,6 @@ import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 
-class Point2D {
-
-    double x, y;
-
-    public Point2D(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
-}
-
 public class GenerateBezierCurve {
 
     // gets a point in a bezier line
