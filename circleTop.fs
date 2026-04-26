c0060be3ad90072f1482f49b1df0bd6cbad80df2
thisotherguyalso <161091156+thisotherguyalso@users.noreply.github.com>
2026-04-27 00:11:33 +0800

    edited some parts

--- circleTop.fs
+++ circleTop.fs
@@ -12,17 +12,18 @@ out vec4 fragmentColor;
 
 void main() {
   vec3 lightPosition = vec3(2.0, 2.0, 0.0);
-  vec3 l = normalize(vec3(1.0, 1.0, 0.5));
+  vec3 l = normalize(lightPosition - worldSpacePosition);
   vec3 n = normalize(worldSpaceNormal);
   float diffuseColor = max(dot(n, l), 0.0);
-  float ambientColor = 0.3f;
+  float ambientColor = 0.4f;
 
   vec3 viewDir = normalize(cameraPos - worldSpacePosition);
   vec3 reflectDir = reflect(-l, n);
   float shine = 64.0;
   float specularLight = pow(max(dot(viewDir, reflectDir), 0.0), shine);
   float specularStrength = 0.6;
-  vec4 texColor = texture(top_texture, shaderUV);
   float lighting = ambientColor + diffuseColor + specularStrength * specularLight;
+
+  vec4 texColor = texture(top_texture, shaderUV);
   fragmentColor = texColor * vec4(lighting * objectColor, 1.0f);
 }
